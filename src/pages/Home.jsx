0a00739d987937d5220576c556c36d11cc470a4d
Yelda-ozbek <yeldayazilim@icloud.com>
2025-01-27 21:56:06 +0300

    Anasayfa Oluşturuldu.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "./compontents/Hero";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Hero />
+        </div>
+    );
+};
+
+
+export default Home ;
