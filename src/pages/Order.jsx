fb9734c8c1914105698d0daaa089c784cd7cb8b5
Yelda-ozbek <yeldayazilim@icloud.com>
2025-01-27 21:01:47 +0300

    Order sayfasına form ekleme tamamlandı.

---
+++ src/pages/Order.jsx
@@ -0,0 +1,9 @@
+import OrderForm from "../compontents/OrderForm";
+const Order = () => {
+    return (
+        <div className="order-page">
+        <h1>Pizza Siparişi</h1>
+        <OrderForm />
+        </div>
+    );
+};
